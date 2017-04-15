b2310c686039f6dd915d6bdc8b99b6fd3ac15e62
itchyny <itchyny@hatena.ne.jp>
2017-04-15 23:28:07 +0900

    maze v0.0.1

--- maze.rb
+++ maze.rb
@@ -1,13 +1,13 @@
 class Maze < Formula
   homepage 'https://github.com/itchyny/maze'
-  version '0.0.0'
+  version '0.0.1'
 
   if Hardware::CPU.is_64_bit?
-    url 'https://github.com/itchyny/maze/releases/download/v0.0.0/maze_darwin_amd64.zip'
-    sha256 '857c560b24380060f2674472c6d9a47d8fc75c59cf334c440a7797e1c69ef88c'
+    url 'https://github.com/itchyny/maze/releases/download/v0.0.1/maze_darwin_amd64.zip'
+    sha256 'dc2cf6045a9191edccf6192f6f5af09510feebbbb6625da4f376cb4010c027c9'
   else
-    url 'https://github.com/itchyny/maze/releases/download/v0.0.0/maze_darwin_386.zip'
-    sha256 'efb43eda13e8bf8aa86bed53d99fddc414947999ba65e27bc4da8dfe7d2eabe0'
+    url 'https://github.com/itchyny/maze/releases/download/v0.0.1/maze_darwin_386.zip'
+    sha256 '4f1e6d6874fc460023ab638c692e2a0d1f12cdcc73eb9da2bfe2ad7980a1c4c6'
   end
 
   head do
